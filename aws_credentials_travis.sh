1214b60a7693a0c8d0f3c709e603d38cab978394
Kyle Wilkins <kawilki3@asu.edu>
2019-11-09 17:19:43 -0500

    updating with credentials

--- aws_credentials_travis.sh
+++ aws_credentials_travis.sh
@@ -4,6 +4,6 @@ mkdir -p ~/.aws
 
 cat > ~/.aws/credentials << EOL
 [default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws_access_key_id = AKIATYNYETND76I47P64
+aws_secret_access_key = 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
 EOL
