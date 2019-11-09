9494153ee7776f8f029a7c824e32c55738b2d0cf
Kyle Wilkins <kawilki3@asu.edu>
2019-11-09 18:09:36 -0500

    now with ask_config

--- aws_credentials_travis.sh
+++ aws_credentials_travis.sh
@@ -10,4 +10,4 @@ aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
 EOL
 
 sed -e s/ASK_ACCESS_TOKEN/${ASK_ACCESS_TOKEN}/g -e \
-    s/ASK_REFRESH_TOKEN/${ASK_REFRESH_TOKEN}/g sample_ask_config.json > ~/.ask/cli_config
+    s/ASK_REFRESH_TOKEN/${ASK_REFRESH_TOKEN}/g ask_config.json > ~/.ask/cli_config
